ec8294944e649ce17fec445be7fbb146d9f4927a
fulleni <fulleni@hotmail.com>
2025-04-02 05:34:44 +0100

    chore: initial commit

---
+++ lib/src/ht_categories_client.dart
@@ -0,0 +1,7 @@
+/// {@template ht_categories_client}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class HtCategoriesClient {
+  /// {@macro ht_categories_client}
+  const HtCategoriesClient();
+}
---
+++ test/src/ht_categories_client_test.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:ht_categories_client/ht_categories_client.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('HtCategoriesClient', () {
+    test('can be instantiated', () {
+      expect(HtCategoriesClient(), isNotNull);
+    });
+  });
+}
